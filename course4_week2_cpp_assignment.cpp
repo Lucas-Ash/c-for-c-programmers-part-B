077fb7ef743984adbbfed518656c14fd0f8dba2d
Lucas Ash <lucas.ash.567@gmail.com>
2024-09-13 00:41:31 +0100

    HexBoard class

--- course4_week2_cpp_assignment.cpp
+++ course4_week2_cpp_assignment.cpp
@@ -8,4 +8,12 @@ int main() {
     // We want to generate a hex board
     // We need to create a graph class
 
-    
+    class HexBoard {
+
+        public:
+            // Constructor
+            HexBoard(int n) {}
+
+
+    return 0;
+    }    
